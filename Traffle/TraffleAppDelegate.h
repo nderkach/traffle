8028b28fba7baabda88beae2bd8dd93a0946c419
Nikolay Derkach <nderk@me.com>
2014-05-18 15:42:36 -0700

    Initial Commit

---
+++ Traffle/TraffleAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TraffleAppDelegate.h
+//  Traffle
+//
+//  Created by Nikolay Derkach on 18/05/14.
+//  Copyright (c) 2014 Nikolay Derkach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TraffleAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Traffle/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Traffle
+//
+//  Created by Nikolay Derkach on 18/05/14.
+//  Copyright (c) 2014 Nikolay Derkach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TraffleAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TraffleAppDelegate class]));
+    }
+}
